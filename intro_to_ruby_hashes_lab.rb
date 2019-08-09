69e73b9e52a42aabe65f6c7d30b8eebbb14eec87
Tanya Bihag <tbihag@gmail.com>
2019-08-09 03:53:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,4 +1,9 @@
-def base_hash
+def countries_and_capitals_of_the_world = {
+  :north_america => {
+    "Canada" => {
+      :capital => "Ottawa",
+      :capital_climate => "Kőppen Dfb"
+    }
   
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
